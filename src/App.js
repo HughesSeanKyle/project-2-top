5182679bc274cf5d88aefef65dbec7a2717204a5
HughesSeanKyle <khughessean@yahoo.com>
2023-01-24 19:10:00 +0200

    Setup theme colors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
+/*
+Three main components needed here 
+1. Sidenav 
+2. Nav 
+3. layout component 
+*/
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <div className="App">My dashboard</div>;
 }
 
 export default App;

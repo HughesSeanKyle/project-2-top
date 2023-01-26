ee13313c6c7e5994d6b36eb52abac53f9ec203aa
HughesSeanKyle <khughessean@yahoo.com>
2023-01-26 15:26:10 +0200

    Final project code

--- src/components/Cards/Announcements.js
+++ src/components/Cards/Announcements.js
@@ -4,6 +4,28 @@ function Announcements() {
 	return (
 		<>
 			<h3 className="aside-heading">Announcements</h3>
+			<div className="aside-content-container">
+				<div className="announcement-card">
+					<h5 className="card-heading">Site Maintenance</h5>
+					<p style={{ fontSize: '14px' }}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+						mollitia, molestiae quas vel sint commodi.
+					</p>
+					<hr style={{ margin: '18px 0px' }} className="divider-style" />
+					<h5 className="card-heading">Community Share Day</h5>
+					<p style={{ fontSize: '14px' }}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+						mollitia, molestiae quas vel sint commodi.
+					</p>
+					<hr style={{ margin: '18px 0px' }} className="divider-style" />
+					<h5 className="card-heading">Updated Privacy Policy</h5>
+					<p style={{ fontSize: '14px' }}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+						mollitia, molestiae quas vel sint commodi.
+					</p>
+					<hr style={{ margin: '18px 0px' }} className="divider-style" />
+				</div>
+			</div>
 		</>
 	);
 }
--- src/components/Cards/Trending.js
+++ src/components/Cards/Trending.js
@@ -4,6 +4,91 @@ function Trending() {
 	return (
 		<>
 			<h3 className="aside-heading">Trending</h3>
+			<div className="aside-content-container">
+				<div className="announcement-card">
+					<div
+						style={{
+							display: 'flex',
+							justifyContent: 'flex-start',
+							alignItems: 'center',
+							marginTop: '18px',
+						}}
+					>
+						<img
+							src="./niftyswap-assets/nft-two-no-bg-trimmy.png"
+							alt="profile-pic"
+							width="36px"
+							height="36px"
+							style={{ marginRight: '18px' }}
+						/>
+
+						<div className="user-info">
+							<h6>@tegan</h6>
+							<p style={{ opacity: '0.5' }}>World Peace Builder</p>
+						</div>
+					</div>
+					<div
+						style={{
+							display: 'flex',
+							justifyContent: 'flex-start',
+							alignItems: 'center',
+							marginTop: '18px',
+						}}
+					>
+						<img
+							src="./niftyswap-assets/nft-one-prim-no-bg-trimmy.png"
+							alt="profile-pic"
+							width="36px"
+							height="36px"
+							style={{ marginRight: '18px' }}
+						/>
+						<div className="user-info">
+							<h6>@morgan</h6>
+							<p style={{ opacity: '0.5' }}>Super Cool Project</p>
+						</div>
+					</div>
+					<div
+						style={{
+							display: 'flex',
+							justifyContent: 'flex-start',
+							alignItems: 'center',
+							marginTop: '18px',
+						}}
+					>
+						<img
+							src="./niftyswap-assets/nft-three-no-bg-trimmy.png"
+							alt="profile-pic"
+							width="36px"
+							height="36px"
+							style={{ marginRight: '18px' }}
+						/>
+						<div className="user-info">
+							<h6>@kendall</h6>
+							<p style={{ opacity: '0.5' }}>Life Changing App</p>
+						</div>
+					</div>
+					<div
+						style={{
+							display: 'flex',
+							justifyContent: 'flex-start',
+							alignItems: 'center',
+							marginTop: '18px',
+						}}
+					>
+						<img
+							src="./niftyswap-assets/nft-four-no-bg-trimmy.png"
+							alt="profile-pic"
+							width="36px"
+							height="36px"
+							style={{ marginRight: '18px' }}
+						/>
+						<div className="user-info">
+							<h6>@alex</h6>
+							<p style={{ opacity: '0.5' }}>No Traffic Maker</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
